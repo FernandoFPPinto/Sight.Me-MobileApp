ba1c9e27a85d9f307ed932652aa4e7a3a960f053
FernandoFPPinto <fernando_fp_pinto@hotmail.com>
2015-01-30 16:59:25 +0000

    Added iBeacon Plugin Implementation

--- Social Network Mobile Client/www/js/BeaconRegion.js
+++ Social Network Mobile Client/www/js/BeaconRegion.js
@@ -6,14 +6,15 @@
 function createBeacon() {
 
     var uuid = 'DA5336AE-2042-453A-A57F-F80DD34DFCD9'; // mandatory
-    var identifier = 'beaconOnHuaweiG6'; // mandatory
+    var identifier = 'beaconOnTheMacBooksShelf'; // mandatory
     var minor = 1000; // optional, defaults to wildcard if left empty
     var major = 5; // optional, defaults to wildcard if left empty
 
     // throws an error if the parameters are not valid
     var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
 
-    return beaconRegion;   
-} 
+    return beaconRegion;
+
+}
 
 
