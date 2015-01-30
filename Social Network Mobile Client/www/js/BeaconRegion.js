8e0be6682b05acc6eb3434cc7f480094fe346945
FernandoFPPinto <fernando_fp_pinto@hotmail.com>
2015-01-30 16:19:28 +0000

    Added New PhoneGap Project

---
+++ Social Network Mobile Client/www/js/BeaconRegion.js
@@ -0,0 +1,19 @@
+/**
+ * Function that creates a BeaconRegion data transfer object.
+ * 
+ * @throws Error if the BeaconRegion parameters are not valid.
+ */
+function createBeacon() {
+
+    var uuid = 'DA5336AE-2042-453A-A57F-F80DD34DFCD9'; // mandatory
+    var identifier = 'beaconOnHuaweiG6'; // mandatory
+    var minor = 1000; // optional, defaults to wildcard if left empty
+    var major = 5; // optional, defaults to wildcard if left empty
+
+    // throws an error if the parameters are not valid
+    var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
+
+    return beaconRegion;   
+} 
+
+
